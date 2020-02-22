189ebe8d832568ae7ca52ca4d6c85e16a9653b0c
kozistr <kozistr@gmail.com>
2020-02-23 01:27:20 +0900

    feature: add metric types

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -11,3 +11,13 @@ class DeviceType(str, Enum):
 class ModelType(str, Enum):
     INCEPTION_V2 = 'inception_v2'
     INCEPTION_V3 = 'inception_v3'
+
+
+@unique
+class MetricType(str, Enum):
+    IS = 'inception_score'
+    FID = 'fid'
+    LPIPS = 'lpips'
+    PPL = 'ppl'
+    PSNR = 'psnr'
+    SSIM = 'ssim'
