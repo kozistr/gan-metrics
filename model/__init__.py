e13896dd91b04c866c373f4b2bf00518bc8ac750
kozistr <kozistr@gmail.com>
2020-02-23 01:46:29 +0900

    feature: able to use pooling layer

--- model/__init__.py
+++ model/__init__.py
@@ -4,9 +4,18 @@ from torchvision.models.inception import inception_v3
 from metrics import ModelType
 
 
-def load_model(model_type: str, device: str = 'cpu') -> nn.Module:
+def load_model(model_type: str, use_pooling_layer: bool = False, device: str = 'cpu') -> nn.Module:
+    """
+    loading the model by following model_type parameter
+    :param model_type: str. type of model, default Inception-v3.
+    :param use_pooling_layer: bool. use right after average pooling layer.
+    :param device: str. type of device.
+    :return: nn.Module. model instance.
+    """
     if model_type == ModelType.INCEPTION_V3:
         model = inception_v3(pretrained=True, transform_input=False)
+        if use_pooling_layer:
+            model.fc = nn.Sequential()
     elif model_type == ModelType.INCEPTION_V2:
         raise NotImplementedError(
             f'[-] not implemented model_type : {model_type}'
