3bdc3ec1d7035634385bcdfc4ab9e61cb9041153
kozistr <kozistr@gmail.com>
2020-02-23 17:34:47 +0900

    feature: implement custom dataset

---
+++ data/custom/__init__.py
@@ -0,0 +1,20 @@
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+
+def _get_custom_transform(config):
+    image_size: int = int(config['data']['image_size'])
+    return transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+
+def get_custom_dataset(config) -> Dataset:
+    return ImageFolder(
+        root=config['data']['dataset_path'],
+        transform=_get_custom_transform(config),
+    )
