814f20965e58ef47d625b87a05105257d555dc30
kozistr <kozistr@gmail.com>
2020-02-23 17:42:04 +0900

    feature: add 5 more datasets

--- data/__init__.py
+++ data/__init__.py
@@ -2,6 +2,12 @@ from enum import unique, Enum
 from torch.utils.data import Dataset, DataLoader
 from data.cifar import get_cifar10_dataset, get_cifar100_dataset
 from data.mnist import get_mnist_dataset, get_fashion_mnist_dataset
+from data.lsun import get_lsun_dataset
+from data.custom import get_custom_dataset
+from data.celeba import get_celeba_dataset
+from data.lfw import get_lfw_dataset
+from data.imagenet import get_imagenet_dataset
+
 
 @unique
 class DataSetList(str, Enum):
@@ -27,15 +33,15 @@ def get_dataset(config) -> Dataset:
     elif dataset_type == DataSetList.FashionMNIST:
         dataset = get_fashion_mnist_dataset(config)
     elif dataset_type == DataSetList.CELEBA:
-        dataset = None
+        dataset = get_celeba_dataset(config)
     elif dataset_type == DataSetList.IMAGENET:
-        dataset = None
+        dataset = get_imagenet_dataset(config)
     elif dataset_type == DataSetList.LSUN:
-        dataset = None
+        dataset = get_lsun_dataset(config)
     elif dataset_type == DataSetList.LFW:
-        dataset = None
+        dataset = get_lfw_dataset(config)
     elif dataset_type == DataSetList.CUSTOM:
-        dataset = None
+        dataset = get_custom_dataset(config)
     else:
         raise NotImplementedError(f'[-] not supported dataset : {dataset_type}')
     return dataset
