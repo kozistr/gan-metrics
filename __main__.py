3b83e6561bedff372fb9e9f3abae9cd3898177ec
kozistr <kozistr@gmail.com>
2020-02-29 20:10:59 +0900

    update: more baseline codes

--- __main__.py
+++ __main__.py
@@ -1,13 +1,16 @@
 from data.utils import get_config
-from . import  CONFIG_FILENAME
+from . import CONFIG_FILENAME
 from data import get_data_loader
+from typing import List
 
 
 def main():
     config = get_config(CONFIG_FILENAME)
 
-    real_data_loader = get_data_loader(config)
-    fake_data_loader = get_data_loader(config)
+    real_data_loader = get_data_loader(config, target='real')
+    fake_data_loader = get_data_loader(config, target='fake')
+
+    metrics: List[str] = config['metrics']['metric_type']
 
 
 main()
